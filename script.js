3c24099ef272e65042be98b9fa096ec1b6d31b9f
Ahmed Mikkawe <dev.ahmedmikkawe@gmail.com>
2021-08-11 13:51:02 +0200

    refactor Book to make it uses class instead of plain constructor

--- script.js
+++ script.js
@@ -52,11 +52,13 @@ function reloadBooks(){
     }
     showBooks();
 }
-function Book(title,author,pages,read) {
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.read = read;
+class Book {
+    constructor(title,author,pages,read) {
+        this.title = title;
+        this.author = author;
+        this.pages = pages;
+        this.read = read;
+    }
 }
 
 function addBookToLibrary() {
